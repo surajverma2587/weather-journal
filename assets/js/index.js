a220e6243bf7c2442af9297fd2d9368dffd88a7f
surajverma2587 <surajverma2587@gmail.com>
2022-05-09 23:05:43 +0100

    add time

--- assets/js/index.js
+++ assets/js/index.js
@@ -63,8 +63,8 @@ const renderCurrentData = (data) => {
     <div class="text-center">
       <h2 class="my-2">${data.cityName}</h2>
       <h3 class="my-2">${moment
-        .unix(1652130296)
-        .format("dddd, Do MMM, YYYY")}</h3>
+        .unix(data.weatherData.current.dt + data.weatherData.timezone_offset)
+        .format("dddd, Do MMM, YYYY HH:mm:ss")}</h3>
       <div>
         <img
           src="http://openweathermap.org/img/w/${
